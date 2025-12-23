cad73c236f64e4d3b3fa7ecefae1eb2733b6e3a6
Vlad <amaimon7778@mail.ru>
2025-12-23 17:15:58 +0300

    Add logic check params

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class SimpleEtl():
         self.loaders = {
             'csv':{
                 'class': CSVLoader,
-                'required_params': ['path'],
+                'required_params': ['path', 'main'],
                 'optional_params': {'delimiter': ',', 'encoding': 'utf-8'}
             }
         }
@@ -20,21 +20,27 @@ class SimpleEtl():
 
 
     def check_all_params(self):
-        for i in self._initialized_attrs:
-            print(i)
+        required_params = self.loaders[self.type]['required_params']
+        optional_params = self.loaders[self.type]['optional_params']
+
+        # print(all(elem in self._initialized_attrs for elem in required_params))
+        return all(elem in self._initialized_attrs for elem in required_params)
 
     def run(self):
-        try:
-            loader_class = self.loaders[self.type]
-        except KeyError:
-            # Покажи пользователю, какие типы доступны
-            available = list(self.loaders.keys())
-            raise ValueError(f"Unsupported type '{self.type}'. Available: {available}")
-        except TypeError:
-            required_params = loader_class['required_params']
-            raise ValueError(f"Need required params: {required_params}")
+        if not self.check_all_params():
+            raise ValueError(f"Need required params")
+
+        # try:
+        #     loader_class = self.loaders[self.type]
+        # except KeyError:
+        #     # Покажи пользователю, какие типы доступны
+        #     available = list(self.loaders.keys())
+        #     raise ValueError(f"Unsupported type '{self.type}'. Available: {available}")
+        # except TypeError:
+        #     required_params = loader_class['required_params']
+        #     raise ValueError(f"Need required params: {required_params}")
 
 
 etl = SimpleEtl('csv', path='files/csv1.csv')
 # etl.run()
-etl.check_all_params()
+etl.run()
