77e4ea1f32d2be726a56b5ea86f9eeb490ba0c19
Vlad <amaimon7778@mail.ru>
2025-12-25 10:57:03 +0300

    Add s2t

--- database/database.py
+++ database/database.py
@@ -8,7 +8,23 @@ class Database():
     def close_connection(self):
         self.cursor.close()
         self.conn.close()
+    
+    def create_meta_table(self):
+        drop_loader_type_tbl = """
+        drop table loader_name;
+    """
 
+        create_loader_type_tbl = """
+        create table if not exists loader_name (
+        loader_name string,
+        tech_changed_dttm timestamp
+    );
+    """
+        self.cursor.execute(drop_loader_type_tbl)
+        self.conn.commit()
+        self.cursor.execute(create_loader_type_tbl)
+        self.conn.commit()
+        self.close_connection()
 
 db = Database()
-db.close_connection()
+db.create_meta_table()
